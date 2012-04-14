e3c003ef13e7b2bec9206d674fb5d4b93cfdf894
Anton Nguyen <anton@anton.2ndsiteinc.com>
2012-04-13 20:04:26 -0400

    Custom messages!

--- heartmap/dating/views.py
+++ heartmap/dating/views.py
@@ -11,6 +11,19 @@ def index(request):
 def twilio(request):
 	import twilio.twiml
 
+	callers = {
+		"+16476698275" : "Anton Nguyen",
+		"+16473099891" : "Rae Chao Jin Qu"
+	}
+
+	response = "Hello Monkey!"
 	resp = twilio.twiml.Response()
-	resp.say("Hello Rae!")
+
+	if 'from' in request.GET:
+		from_number = request.GET['from']
+		if from_number in callers:
+			response = "Hello " + callers[from_number]
+
+	resp.say(response)
+
 	return HttpResponse(str(resp), mimetype="application/xml")
